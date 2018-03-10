6b9e727e697133dce559d26f7f474ca793127e0e
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-10 21:07:40 +0800

    [index] Use command-line-args and command-line-usage

--- index.js
+++ index.js
@@ -2,35 +2,122 @@
 
 'use strict';
 
+// Use command-line-args instead of commander.js because need options with multiple values
+// https://github.com/tj/commander.js/issues/674
+const cla = require('command-line-args');
+const clu = require('command-line-usage')
 const pkginfo = require('pkginfo')(module, 'version');
-const program = require('commander');
 const tw = require('taiwan-weather');
 
-const list = val => val.split(',');
-
-program
-	.version(module.exports.version, '-v, --version')
-	.option('-k, --api-key <value>', 'required API key')
-	.option('-l, --loc [items]', 'list of locations to download', list)
-	.option('-f, --freq [items]', 'list of frequencies to download', list)
-	.option('-L, --lang [items]', 'list of languages to download', list)
-	.option('-o, --output [path]', 'output directory')
-	.option('-p, --prefix [value]', 'files prefix')
-	.option('-j, --json', 'convert XML files to JSON files')
-	.option('-V, --verbose', 'verbose mode to display logs')
-	.parse(process.argv);
-
-tw.get(program.apiKey, {
-	loc: program.loc,
-	freq: program.freq,
-	lang: program.lang,
-	output: program.output,
-	prefix: program.prefix,
-	toJson: program.json,
-	debug: program.verbose
-}, (err) => {
-	if (err) {
-		// Use global.__logger from taiwan-weather module
-		__logger.error(err);
+// Define expected options
+// Used by command-line-args and command-line-usage
+const optDef = [{
+		name: 'api-key',
+		alias: 'k',
+		type: String,
+		description: 'Required API key'
+	},
+	{
+		name: 'loc',
+		alias: 'l',
+		type: String,
+		multiple: true,
+		description: 'List of locations to download'
+	},
+	{
+		name: 'freq',
+		alias: 'f',
+		type: String,
+		multiple: true,
+		description: 'List of frequencies to download'
+	},
+	{
+		name: 'lang',
+		alias: 'L',
+		type: String,
+		multiple: true,
+		description: 'List of languages to download'
+	},
+	{
+		name: 'output',
+		alias: 'o',
+		type: String,
+		description: 'Output directory'
+	},
+	{
+		name: 'prefix',
+		alias: 'p',
+		type: String,
+		description: 'Files prefix'
+	},
+	{
+		name: 'json',
+		alias: 'j',
+		type: Boolean,
+		description: 'Convert XML files to JSON files'
+	},
+	{
+		name: 'verbose',
+		alias: 'V',
+		type: Boolean,
+		description: 'Turn on debugging logs'
+	},
+	{
+		name: 'help',
+		alias: 'h',
+		type: Boolean,
+		description: 'Print this usage guide'
+	},
+	{
+		name: 'version',
+		alias: 'v',
+		type: Boolean,
+		description: 'Print version number'
 	}
-});
+];
+
+// Parse input options
+const opt = cla(optDef);
+
+// Convert enum values into technical values
+const loc = opt.loc && opt.loc.map(l => tw.DataEnum.Loc[l]);
+const freq = opt.freq && opt.freq.map(f => tw.DataEnum.Freq[f]);
+const lang = opt.lang && opt.lang.map(l => tw.DataEnum.Lang[l]);
+
+if (opt.version) {
+	// Only display version number
+	console.log(module.exports.version);
+} else if (opt.help) {
+	// Only display usage documentation
+	console.log(clu(
+		[{
+				header: 'taiwan-weather-CLI',
+				content: [
+					'A CLI for taiwan-weather node.js module',
+					'',
+					'Usage: `tw --api-key <api key> [options]`'
+				]
+			},
+			{
+				header: 'Options',
+				optionList: optDef
+			}
+		]
+	));
+} else {
+	// Download files with specified options
+	tw.get(opt['api-key'], {
+		loc: loc,
+		freq: freq,
+		lang: lang,
+		output: opt.output,
+		prefix: opt.prefix,
+		toJson: opt.json,
+		debug: opt.verbose
+	}, (err) => {
+		if (err) {
+			// Use global.__logger from taiwan-weather module
+			__logger.error(err);
+		}
+	});
+}
